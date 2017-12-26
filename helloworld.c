22c5c1bd36cec69773a5773772e4937d6fb630d4
afx <435153132@qq.com>
2017-12-26 14:37:06 +0800

    clear content of v1.0.0.3

--- helloworld.c
+++ helloworld.c
@@ -1,22 +1,13 @@
+
+// hello world
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 // v1.0.0.3
-
-
-#include <stdio.h>
-
-int main() {
-	printf("hello world\n") ;
-	return 0 ;
-}
-
-
-// v1.0.0.3
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
