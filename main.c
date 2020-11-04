23e9aa8c8b00aaadbe07ba7ea2e6f73af6919ff1
teng_fl <250704958@qq.com>
2020-11-04 09:52:26 +0800

    merge v1.0.0.2-squash into v1.0.0.2

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <stdio.h>
+
 void foo() {
 	std::cout << "foo is called" << std::endl;
+}
+
+void bar() {
+	printf("bar is called\n");
 }
 
 // 主函数入口
 int main() {
 	std::cout << "hello world" << std::endl;	
+	foo();
+	bar();
+
 	return 0;
 }
